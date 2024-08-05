ae86524ce0ffffc79ea7fd605316d85105ddfa36
Syed Shah <raedshah1@gmail.com>
2024-08-04 22:06:18 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Syed Shah.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <h1>Syed Shah</h1>
+        <ul>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#resume">Resume</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </div>
+      <div className="content">
+        <section id="projects">
+          <h2>Projects</h2>
+          {/* Add project details here */}
+        </section>
+        <section id="resume">
+          <h2>Resume</h2>
+          {/* Add resume details here */}
+        </section>
+        <section id="about">
+          <h2>About</h2>
+          {/* Add about details here */}
+        </section>
+      </div>
     </div>
   );
 }
